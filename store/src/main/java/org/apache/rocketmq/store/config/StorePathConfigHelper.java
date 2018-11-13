62d7fbc689ebd1b36de4c6c31e13b85d424f4c11
jiangxinlingdu <jiangxinlingdu.com>
2018-11-13 20:33:03 +0800

    m

--- store/src/main/java/org/apache/rocketmq/store/config/StorePathConfigHelper.java
+++ store/src/main/java/org/apache/rocketmq/store/config/StorePathConfigHelper.java
@@ -32,10 +32,12 @@ public class StorePathConfigHelper {
         return rootDir + File.separator + "index";
     }
 
+    //检查点
     public static String getStoreCheckpoint(final String rootDir) {
         return rootDir + File.separator + "checkpoint";
     }
 
+    //中断
     public static String getAbortFile(final String rootDir) {
         return rootDir + File.separator + "abort";
     }
